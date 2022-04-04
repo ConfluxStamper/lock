7e9221f7d477f46372bafd3367628eb72467a2fa
flowfire <ziyi.meng@obsidians.io>
2022-04-04 19:05:30 +0800

    update fluent

--- connectors/conflux-portal.ts
+++ connectors/conflux-portal.ts
@@ -3,7 +3,7 @@ import LockConnector from '../src/connector';
 export default class Connector extends LockConnector {
   async connect() {
     let provider;
-    if (window['conflux'] && window['conflux'].isConfluxPortal) {
+    if (window['conflux']) {
       provider = window['conflux'];
       try {
         await window['conflux'].enable();
